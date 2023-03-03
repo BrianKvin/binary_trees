d380cad68a5d9434c1bf4847913b415f94e911f4
BrianKvin <briankvin@gmail.com>
2023-03-03 23:26:30 +0300

    Insert right

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -38,7 +38,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		new->left = parent->left;
-		parent->left = new;
 		parent->left->parent = new;
 	}
 	/* if parent doesn't have a left child, the new node is set as left child */
